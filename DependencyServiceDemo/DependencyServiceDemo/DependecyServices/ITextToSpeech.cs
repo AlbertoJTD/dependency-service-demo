33398da31487e9881047b0fd1682e1740e03c3e7
AlbertoJTD <albertojtd@outlook.com>
2023-07-08 22:46:59 -0600

    Add ITextToSpeech

---
+++ DependencyServiceDemo/DependencyServiceDemo/DependecyServices/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyServiceDemo.DependecyServices
+{
+	public interface ITextToSpeech
+	{
+		void Speak(string text);
+	}
+}
