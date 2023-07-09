b454721a1a4946a0fda313472ae111909b8eb891
AlbertoJTD <albertojtd@outlook.com>
2023-07-08 23:20:12 -0600

    Add TextToSpeech dependency service to android and iOS

---
+++ DependencyServiceDemo/DependencyServiceDemo.Android/DependecyServices/TextToSpeechImplementation.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Speech.Tts;
+using Android.Views;
+using Android.Widget;
+using DependencyServiceDemo.DependecyServices;
+using DependencyServiceDemo.Droid.DependecyServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeechImplementation))]
+namespace DependencyServiceDemo.Droid.DependecyServices
+{
+	public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+		private TextToSpeech speaker;
+		private string toSpeak;
+
+		public void OnInit([GeneratedEnum] OperationResult status)
+		{
+			speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+		}
+
+		public void Speak(string text)
+		{
+			toSpeak = text;
+
+			if (speaker == null)
+			{
+				speaker = new TextToSpeech(Forms.Context, this);
+			}
+            else
+            {
+				speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+	}
+}
---
+++ DependencyServiceDemo/DependencyServiceDemo.iOS/DependecyServices/TextToSpeechImplementation.cs
@@ -0,0 +1,32 @@
+﻿using AVFoundation;
+using DependencyServiceDemo.DependecyServices;
+using DependencyServiceDemo.iOS.DependecyServices;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeechImplementation))]
+namespace DependencyServiceDemo.iOS.DependecyServices
+{
+	internal class TextToSpeechImplementation : ITextToSpeech
+	{
+		public void Speak(string text)
+		{
+			var speechSynthesizer = new AVSpeechSynthesizer();
+			var speechUtterance = new AVSpeechUtterance(text)
+			{
+				Rate = AVSpeechUtterance.MaximumSpeechRate / 4,
+				Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
+				Volume = 0.5f,
+				PitchMultiplier = 1.0f
+			};
+
+			speechSynthesizer.SpeakUtterance(speechUtterance);
+		}
+	}
+}
